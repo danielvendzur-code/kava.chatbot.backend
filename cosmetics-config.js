2287c4487e3c50d26abd9d830bc67c0c9b9eeadf
danielvendzur-code <daniel.vendzur@gmail.com>
2026-09-09 15:46:36 +0200

    Use exact Barbora Lori initials in chatbot bubble

--- cosmetics-config.js
+++ cosmetics-config.js
@@ -182,8 +182,8 @@
       name:"Barbora Lori", domain:"barboralori.sk", website:"https://www.barboralori.sk/",
       theme:{brand:'#2b2b2b',accent:'#117e6f',soft:'#e9f2f0',paper:'#fdfdfc',ink:'#232323',line:'#dbe7e3'},
       wordmark:"<img class=\"cx-wordmark cx-logo\" src=\"/assets/cosmetics/barboralori-logo.png\" alt=\"Barbora Lori\">",
-      launcherMark:"<img class=\"cx-bubble-logo\" src=\"/assets/cosmetics/barboralori-launcher.svg\" alt=\"Barbora Lori\">",
-      avatarMark:"<img class=\"cx-bubble-logo cx-bubble-logo--avatar\" src=\"/assets/cosmetics/barboralori-avatar.svg\" alt=\"Barbora Lori\">",
+      launcherMark:"<span class=\"cx-barbora-monogram\" aria-label=\"Barbora Lori\"><i class=\"cx-barbora-letter cx-barbora-letter--b\"></i><i class=\"cx-barbora-letter cx-barbora-letter--l\"></i></span>",
+      avatarMark:"<span class=\"cx-barbora-monogram cx-barbora-monogram--avatar\" aria-label=\"Barbora Lori\"><i class=\"cx-barbora-letter cx-barbora-letter--b\"></i><i class=\"cx-barbora-letter cx-barbora-letter--l\"></i></span>",
       hero:'/assets/cosmetics/barboralori.jpg',
       benefit:["Rovnaké fľaštičky prestanú miasť","Menej otázok pred nákupom","Odpovie aj mimo otváracích hodín"],
       products:[
